5af39cd065f031930330af254487000ee2aac987
ARobin2288 <79115206+ARobin2288@users.noreply.github.com>
2021-05-10 17:08:05 -0500

    adjustments

--- src/Main.js
+++ src/Main.js
@@ -8,11 +8,12 @@ export default function Main() {
   return (
     <div className="Main">
       <div className="row">
-        <div className="col-5">
+        <div className="col-7">
           <Location />
+          <br />
           <Extra />
         </div>
-        <div className="col-7">
+        <div className="col-5">
           <Temperature />
         </div>
       </div>
