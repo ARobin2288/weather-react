d939822a72723f8bb88fe7d4b95429681c88f03b
ARobin2288 <79115206+ARobin2288@users.noreply.github.com>
2021-05-24 17:32:38 -0500

    added netlify link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default function App() {
         <Weather defaultCity="Chicago" />
         <footer>
           Coded by Robin A. Open-sourced on{" "} 
-          <a href="https://github.com/ARobin2288/weather-react.git" rel=" noopener noreferrer" id="open-source-link" target="_blank">GitHub</a>
+          <a href="https://github.com/ARobin2288/weather-react.git" rel="noopener noreferrer" id="open-source-link" target="_blank">GitHub</a>
+          {" "}Hosted on <a href="https://loving-morse-9c6639.netlify.app/" rel="noopener noreferrer" id="open-source-link" target="_blank"> Netlify</a>.
         </footer>
       </div>
     </div>
